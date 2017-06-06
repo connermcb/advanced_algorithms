431d88a4b1c27e8252e92e43d312588de9ee55b8
connermcb <connermcb@yahoo.com>
2017-06-05 20:24:32 -0500

    Add find pivot method
    Added and tested method for finding row and col
    indices for matrix pivot.

--- table_method.py
+++ table_method.py
@@ -33,6 +33,14 @@ class TableMethod(object):
         line.extend([0 if i != self.n else 1 for i in range(self.n+2)])
         self.A.append(line)
             
+    def find_pivot(self):
+        col = min([each for each in enumerate(self.A[-1][:-1]) if each[1] < 0], key=lambda x:x[1])[0]
+        possible_rows = enumerate([self.A[i][-1]/float(self.A[i][col]) for i in range(self.n)])
+        row = min([each for each in possible_rows if each[1] > 0], key=lambda x:x[1])[0]
+        print(row, col)
+
+
+        
 l = [[4, 3, 1], [1, 1, 1], [2, 1, -1]]
 t = TableMethod(3,3)
 for each in l:
@@ -43,7 +51,7 @@ print(t.A)
 t.add_rhs([3, 10, 10])
 t.add_objective([2, -3, 4])
 t.get_tableau()
-    
+print(t.find_pivot())
     
         
     
